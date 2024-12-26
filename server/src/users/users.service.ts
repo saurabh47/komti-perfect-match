a7ce52dfd149b9d8000da187c2c720b594769664
saurabhgangamwar <saurabhgangamwar78@gmail.com>
2024-12-26 22:18:44 +0530

    - Added Users Service

---
+++ server/src/users/users.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserSession } from 'src/entities/user-session.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(UserSession)
+    private readonly userSessionRepository: Repository<UserSession>,
+  ) {}
+
+  async createSession(selectedGender: string): Promise<UserSession> {
+    const newSession = this.userSessionRepository.create({ selectedGender });
+    return this.userSessionRepository.save(newSession);
+  }
+
+  async getSessionDetails(sessionId: number): Promise<UserSession> {
+    return this.userSessionRepository.findOneOrFail({ where: { sessionId } });
+  }
+}
