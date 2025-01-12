e8299d091023ac6d2e644385f94db05350b07631
saurabhgangamwar <saurabhgangamwar78@gmail.com>
2025-01-13 00:04:43 +0530

    - Refactored profile component & fixed formatting

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,33 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../users.service';
-import { AsyncPipe, NgClass } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { ProfileCardComponent } from '../profile-card/profile-card.component';
 
 @Component({
-  selector: 'app-profile',
-  imports: [AsyncPipe, NgClass, IonicModule, ProfileCardComponent],
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss'],
+    selector: 'app-profile',
+    imports: [AsyncPipe, IonicModule, ProfileCardComponent],
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent  implements OnInit {
-  profile$;
-  constructor(private usersService: UsersService) { 
-    this.profile$ = this.usersService.getProfile();
-  }
-
-  ngOnInit() {
-
-  }
-
-  toggleDetails(profile: any): void {
-    if(profile.user_id != -1) {
-      profile.showDetails = !profile.showDetails;
+export class ProfileComponent implements OnInit {
+    profile$;
+    constructor(private usersService: UsersService) {
+        this.profile$ = this.usersService.getProfile();
     }
-  }
 
-  logout() {
-    this.usersService.logout();
-  }
+    ngOnInit() {}
 
+    toggleDetails(profile: any): void {
+        if (profile.user_id != -1) {
+            profile.showDetails = !profile.showDetails;
+        }
+    }
+
+    logout() {
+        this.usersService.logout();
+    }
 }
